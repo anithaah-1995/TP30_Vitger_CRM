57aec7e6e724e5f76f6dd2800a71b86d4219fb4b
NAGARAJ <NAGARAJ@DESKTOP-IODHK5L>
2025-12-27 12:47:11 +0530

    done!!!!

--- src/test/java/practice_Scripts/DemoTest.java
+++ src/test/java/practice_Scripts/DemoTest.java
@@ -5,6 +5,7 @@ public class DemoTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("---Good morning---Anitha to Azmath----");
+System.out.println("Azmath to Anitha");
 	}
 
 }
